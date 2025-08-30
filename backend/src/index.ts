f05277904d5e0efcb4ce97204ebb969c2b92af7d
wojnan <mr.debil@gmail.com>
2025-08-30 17:53:22 +0200

    route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import { initDB, pool } from "./db";
+import express, { Application, Request, Response } from 'express';
+import authRoutes from './routes/authRoutes';
+import errorHandler from './middlewares/errorHandler';
 
-async function main() {
+
+ /*async function main() {
   await initDB();
 
   const res = await pool.query(
@@ -10,5 +14,17 @@ async function main() {
 
   await pool.end(); 
 }
+main();*/
 
-main();
+const app: Application = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+app.use('/api', authRoutes);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`xServer is running on http://localhost:${port}`);
+});
